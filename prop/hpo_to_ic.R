b68c358b818ec39c1085b3e45aab810fc20369c7
Dave Tang <me@davetang.org>
2017-05-12 14:08:40 +0800

    Use local OBO

--- prop/hpo_to_ic.R
+++ prop/hpo_to_ic.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 #
-# Usage: omim_to_closest.R <HPO ID> [HPO ID]
+# Usage: hpo_to_ic.R <HPO ID> [HPO ID]
 #
 
 args <- commandArgs(TRUE)
@@ -20,20 +20,13 @@ script.basename <- dirname(script.name)
 
 load(paste(script.basename, '/information_content.Robj', sep=''))
 
-# initialise empty data frame to store results
-my_result <- data.frame(id = character(0), info = numeric(0), freq = numeric(0), stringsAsFactors = FALSE)
-
-for (i in 1:length(args)){
-   my_hpo <- args[i]
-   r <- subset(information_content, id == my_hpo)
-   # if there is a match
-   if (nrow(r) > 0){
-      my_result[i,] <- r
-   }
-}
+my_index <- match(args, information_content$id)
+my_result <- information_content[na.exclude(my_index),]
 
 library(ontologyIndex)
-data("hpo")
+# data("hpo")
+hpo <- get_ontology(file = paste(script.basename, "/../data/hp.obo.gz", sep=''))
 
 my_result$name <- sapply(X = my_result$id, FUN = function(x) get_term_property(hpo, property_name = 'name', term = x))
 print(my_result)
+
